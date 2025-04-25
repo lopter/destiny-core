b50bc3c7b73e83b4d90092642ac11d389f53685d
Louis Opter <louis@opter.org>
2025-04-25 13:24:26 -0700

    blogon: fix post titles alignement and update /about

--- library/rust/blogon/app/src/pages/about.rs
+++ library/rust/blogon/app/src/pages/about.rs
@@ -13,7 +13,7 @@ pub fn Index() -> impl IntoView {
                 r#"Hi, I am an open-source contributor, with a heavy startup experience, I worked at large companies in the "Silicon Valley"."#
             </p>
             <p>
-                r#"I take interest in both learning and teaching, writing readable and dependable code. I like writing, and at time of writing, I intend to tell you about Clan, a framework to manage a small, distributed, fleet of NixOS servers. [My first post] goes towards Python, a language I started using in the early years of my long experience with C."#
+                "I take interest in both learning and teaching, writing readable and dependable code. I like writing, and at time of writing, I intend to tell you about Clan, a framework to manage a small, distributed, fleet of NixOS servers." <a href="/blog/0002-fly-io-sidecars-and-extra-luggage">"My first post"</a> " goes towards how I used Nixpkgs to do some edge networking on fly.io."
             </p>
             <p>
                 r#"The artwork on this site is realized by "# <a href="https://www.atelierpentosaurus.com/">"Atelier Pentosaurus"</a> " resident Hugues Opter."
