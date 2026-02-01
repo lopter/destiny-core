b76b88c741259d8c3c932c1ed2e440a16ab447dc
Louis Opter <louis@opter.org>
2026-02-01 05:17:57 +0000

    backups: add --ignore-missing-paths option to `validate-config`

--- library/python/backups/clan_destiny/backups/__main__.py
+++ library/python/backups/clan_destiny/backups/__main__.py
@@ -1,4 +1,5 @@
 import click
+import collections
 import logging
 import pydantic
 import socket
@@ -39,12 +40,39 @@ def main(ctx: click.Context, config_path: Path) -> None:
     default=socket.getfqdn(),
     show_default=True,
 )
+@click.option(
+    # Useful to validate the config in `checkPhase` when generating the
+    # configuration at build time with `writeTextFile`.
+    "--ignore-missing-paths/--no-ignore-missing-paths",
+    help="Ignore validation errors for file that don't exists",
+    default=False,
+    show_default=True,
+)
 @click.pass_context
-def validate_config(ctx: click.Context, fqdn: str) -> None:
+def validate_config(
+    ctx: click.Context,
+    fqdn: str,
+    ignore_missing_paths: bool,
+) -> None:
     try:
         _ = config.load(ctx.obj, fqdn)
-    except pydantic.ValidationError as ex:
-        print(f"Invalid config for {fqdn}: {ex}")
+    except pydantic.ValidationError as exc:
+        errors = exc.errors()
+        if ignore_missing_paths:
+            def missing_path(etype: str) -> bool:
+                return etype == "path_not_file" or etype == "path_not_directory"
+            errors = [err for err in errors if not missing_path(err["type"])]
+        if not errors:
+            return
+        click.echo(f"{len(errors)} validation errors in config for {fqdn}:")
+        errors_by_loc = collections.defaultdict(list)
+        for err in errors:
+            loc = ".".join(str(component) for component in err["loc"])
+            errors_by_loc[loc].append(err)
+        for loc in errors_by_loc:
+            click.echo(f"- {loc}:")
+            for err in errors_by_loc[loc]:
+                click.echo(f"  - {err["msg"]} (input_value={err["input"]})")
         ctx.exit(1)
 
 
